8588175057034ab547b3f328d1646ecd486bed1d
sidprogram <siddheshbhandge9691@gmail.com>
2022-11-11 00:19:31 +0530

    create

---
+++ src/main/java/com/example/demo/TestfileApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestfileApplication.class, args);
+	}
+
+}
